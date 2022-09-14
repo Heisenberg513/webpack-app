8ba71f671bcfaaaa42b80ff3aed0f2a9cf0c96dd
Heisenberg513 <yuans0513@gmail.com>
2022-09-14 16:18:36 +0800

    chore: webpack template

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    process.env.NODE_ENV === 'development' &&
+      require.resolve('react-refresh/babel'),
+    ['@babel/plugin-proposal-decorators', { legacy: true }]
+  ].filter(Boolean),
+  // 预设执行顺序由右往左,所以先处理ts,再处理jsx
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage', // 根据配置的浏览器兼容,以及代码中使用到的api进行引入polyfill按需添加
+        corejs: 3 // 配置使用core-js低版本
+      }
+    ],
+    '@babel/preset-react',
+    '@babel/preset-typescript'
+  ]
+};
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '@/App.less'
+import test from '@/assets/test.jpg'
+
+const App = () => {
+  return (
+    <>
+      <div>bad webpack SHIT</div>
+      <h1>test</h1>
+      <img src={test} alt='test' />
+    </>
+  )
+}
+
+export default App
